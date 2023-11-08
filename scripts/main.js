7f05658610872ebab2bf1e421a0a0768ee87833f
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-08 17:52:39 +0200

    [core]: get value after form submit

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,35 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   modal.close();
 });
+
+// get submit and handle submit task
+
+const submitButton = document.getElementById("form-task");
+
+submitButton.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const taskTitle = document.getElementById("task-title");
+  const taskDescription = document.getElementById("task-description");
+  const taskDueDate = document.getElementById("task-due-date");
+  const taskPriority = document.getElementById("task-priority");
+
+  let taskTitleValue = taskTitle.value;
+  let taskDescriptionValue = taskDescription.value;
+  let taskDueDateValue = taskDueDate.value;
+  let taskPriorityValue = taskPriority.value;
+
+  console.table({
+    taskTitle: taskTitleValue,
+    taskDescription: taskDescriptionValue,
+    taskDueDate: taskDueDateValue,
+    taskPriority: taskPriorityValue,
+  });
+
+  taskTitle.value = "";
+  taskDescription.value = "";
+  taskDueDate.value = "";
+  taskPriority.value = "";
+
+  modal.close();
+});

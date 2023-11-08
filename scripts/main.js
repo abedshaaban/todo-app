39e9a9fb6dd12d059b226ff407f3c2e668ecc403
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-08 18:43:37 +0200

    [core]: load tasks tasks in home page

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,50 @@ if (typeof Storage !== "undefined") {
   const savedTasks = JSON.parse(localStorage.getItem("tasks-list"));
 
   console.log(savedTasks);
+
+  const feed = document.getElementById("feed");
+
+  for (let i = 0; i < savedTasks.length; i++) {
+    const task = savedTasks[i];
+
+    feed.innerHTML += `<div class="task flex-row">
+    <div class="drag-task-handler center pointer">
+      <img src="./public/drag-handle-dots.svg" alt="" />
+    </div>
+
+    <div class="task-content flex-col">
+      <div class="task-head flex-row">
+        <h2>${task?.title}</h2>
+
+        <div class="task-head-actions flex-row">
+          <span class="task-head-actions-priority ${
+            task?.priority == "High"
+              ? "priority-high"
+              : task?.priority == "Medium"
+              ? "priority-medium"
+              : "priority-low"
+          }">
+          ${task?.priority}
+          </span>
+
+          <span>${task?.dueDate}</span>
+
+          <button class="task-head-actions-btn center bg-green">
+            <img src="./public/check.svg" alt="check icon" />
+          </button>
+
+          <button class="task-head-actions-btn center bg-red">
+            <img src="./public/trash.svg" alt="trash icon" />
+          </button>
+        </div>
+      </div>
+
+      <div class="task-description">
+        <p>${task?.description}</p>
+      </div>
+    </div>
+  </div>`;
+  }
 }
 
 // show-hide input text
